429cf636f424fb593ea39f5c04a1af12154992fa
eannam <eannamorley@gmail.com>
2024-01-24 18:46:02 +0000

    added posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 import { ReactFlowProvider } from "reactflow";
 
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <ReactFlowProvider>
-      <Component {...pageProps} />
-    </ReactFlowProvider>
+    <PostHogProvider client={posthog}>
+      <ReactFlowProvider>
+        <Component {...pageProps} />
+      </ReactFlowProvider>
+    </PostHogProvider>
   );
 }
